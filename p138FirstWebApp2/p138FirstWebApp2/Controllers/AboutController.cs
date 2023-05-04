6f64af70b2d62c2b63fa9829d033693d7879a7a4
Kenan <kananmh@code.edu.az>
2023-05-05 00:27:26 +0400

    back

---
+++ p138FirstWebApp2/p138FirstWebApp2/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace p138FirstWebApp2.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult test()
+        {
+            return View();
+        }
+    }
+}
---
+++ p138FirstWebApp2/p138FirstWebApp2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace p138FirstWebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            return View();
+
+
+            //if(id % 2 == 0)
+            //{
+            //    //ContentResult contentResult  = new ContentResult();
+            //    //contentResult.Content= $"salam {id}";
+
+            //    //return contentResult;
+
+            //    return Content($"salam {id}");
+            //}
+            //else if(true)
+            //{
+            //    //ViewResult viewResult = new ViewResult();
+            //    //viewResult.ViewName= "p138";
+
+            //    //return viewResult;
+
+            //    return View("p138");
+            //}
+            //else
+            //{
+            //    return Json("");
+            //}
+
+
+            //ContentResult contentResult  = new ContentResult();
+            //contentResult.Content= $"salam {name} {age}";
+
+            //return contentResult;
+        }
+    }
+}
